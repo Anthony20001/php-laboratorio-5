0c688133fcf3bb7b7dc48c13dd709202a7f1939c
Anthony20001 <ledezmaanthony621@gmail.com>
2022-11-06 19:44:37 -0500

    Add: mvc complete

---
+++ views/templates/navbar.php
@@ -0,0 +1,33 @@
+<?php require_once "utils/security.php"?>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Navbar</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php" aria-current="page">Home<span class="visually-hidden">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Comentario</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <?php $controller = Security::encode("Contact"); $method = Security::encode("create"); ?>
+                        <a class="dropdown-item" href="<?php echo "index.php?c=".$controller."&m=".$method;?>">Crear</a>
+                        <a class="dropdown-item" href="#">Ver</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="d-flex my-2 my-lg-0">
+                <input class="form-control me-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+  </div>
+</nav>
